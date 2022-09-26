a726d8d0d01bf99c389a84457db84f015f9b8b85
eloycotoalfonso <eloycoal@gmail.com>
2022-09-26 15:07:20 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// alert('Hello World!');
+/*let myName = 'Bob';
+document.write(myName);
+document.write(bla);
+
+let myName ='Bob';
+document.write('\n', myName);
+myName = 'John Doe';
+document.write('\n', myName);
+
+// Mathematical expresions
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+*/
+let size = 100;
+let doubleSize = 2 * size;
+// document.write(doubleSize);
+let minSize = doubleSize * 2 - size/2;
+document.write(minSize);
+
+
+
+
+
