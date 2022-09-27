3b6fb1e7603c2a12f8ab60ffa7ae2e1e5a54fb25
eloycotoalfonso <eloycoal@gmail.com>
2022-09-27 13:23:59 +0200

    changed items for exercise2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-alert('Hello World!');
+// alert('Hello World!');
 /*let myName = 'Bob';
 document.write(myName);
 document.write(bla);
@@ -18,6 +18,36 @@ let doubleSize = 2 * size;
 let minSize = doubleSize * 2 - size/2;
 document.write(minSize);
 */
-
+/*
 let favouriteFood = 'Salad';
 document.write(favouriteFood);
+
+let escapedText = "He said: \"Yes!\", that's for sure.";
+let escapedText2 = 'He said: "Yes!", that\'s for sure.';
+console.log(escapedText);
+console.log(escapedText2);
+
+let name = 'John';
+let welcomeMessage1 = 'Hello, I\'m';
+let welcomeMessage2 = 'and I say to you: "Hello!"';
+console.log(welcomeMessage1 + ' ' + name + ' ' + welcomeMessage2);
+// This is a template literal basically you need to put backticks and then write the vatiables with ${} structures
+console.log(`${welcomeMessage1} ${name} ${welcomeMessage2}`);
+
+*/
+
+
+// Objects
+
+let annesAge = 27;
+      let anne = {
+        name: 'Anne',
+        age: annesAge,
+        child: {
+          name: 'Joe',
+          age: 2
+        }
+      };
+       // Add your code (such as console.log(anne.name);) bottom right window(console): 
+let x = [1, 2, 3];
+console.log[x];
