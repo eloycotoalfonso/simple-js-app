66e65ac602fafc34187d846af5eeb3874ae3e8de
eloycotoalfonso <eloycoal@gmail.com>
2022-09-27 13:53:52 +0200

    Added an alternative way to the exercise
    I also corrected some variables names to  fit better in the camelCase standars

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,21 @@ let pokemonsList = [];
 
 
 //creating the objects for each of the pokemons
-let Pokemon1 = {
+let pokemon1 = {
     name: 'Butterfree',
     height: 1.1,
     type: ['bug', 'flying']
 
 }
 
-let Pokemon2 = {
+let pokemon2 = {
     name: 'Geodude',
     height: 0.4,
     type: ['rock', 'ground']
 
 }
 
-let Pokemon3 = {
+let pokemon3 = {
     name: 'Magneton',
     height: 1,
     type: ['electric', 'steel']
@@ -25,9 +25,16 @@ let Pokemon3 = {
 }
 
 //Including pokemons into the array
-pokemonsList.push(Pokemon1);
-pokemonsList.push(Pokemon2);
-pokemonsList.push(Pokemon3);
+pokemonsList.push(pokemon1);
+pokemonsList.push(pokemon2);
+pokemonsList.push(pokemon3);
+
+
+/*Alternative way of adding the pokemons to the array
+pokemonsList[0] = pokemon1;
+pokemonsList[1] = pokemon2;
+pokemonsList[2] = pokemon3;
+*/
 
 //Display the arrays of pokemons
 console.log(pokemonsList);
