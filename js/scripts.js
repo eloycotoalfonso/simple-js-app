afe39ef36e3ac9c109030c07bc802a9e5e23b5ba
eloycotoalfonso <eloycoal@gmail.com>
2022-10-04 12:59:17 +0200

    included forEach loop
    comented the previous loop and include the information message with the forEach loop method.

--- js/scripts.js
+++ js/scripts.js
@@ -39,6 +39,9 @@ pokemonsList[2] = pokemon3;
 //Display the arrays of pokemons
 // console.log(pokemonsList);
 
+/*
+    //////This is commented to change it for a forEach function//////
+
 //Loop that iterates ovr the pokemonList items:
 for (let i = 0; i < pokemonsList.length; i++){
     if(pokemonsList[i].height > 1 ){
@@ -48,3 +51,20 @@ for (let i = 0; i < pokemonsList.length; i++){
         document.write(`${pokemonsList[i].name} (height: ${pokemonsList[i].height}) <br>`);
     }
 }
+
+*/
+
+pokemonsList.forEach(function (pokemon){
+   
+    if(pokemon.height > 1 ){
+        document.write(pokemon.name + ' (height: ' + pokemon.height + ') - Wow! That\'s big!!! <br>');
+    }else{
+        // document.write(pokemonsList[i].name + ' (height: ' + pokemonsList[i].height + ') \n');
+        document.write(`${pokemon.name} (height: ${pokemon.height}) <br>`);
+    }
+    
+});
+
+
+//Adding the for.each function
+
